e9a4c4b0d58e98ae9f39546ef3db121b9ea6e90b
Ace <kalaitzisemmanouil@gmail.com>
2024-03-23 13:15:19 +0100

    Add new method to repository

--- src/main/java/com/example/zapbites/Customer/CustomerRepository.java
+++ src/main/java/com/example/zapbites/Customer/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.example.zapbites.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
 }
--- src/main/java/com/example/zapbites/CustomerAddress/CustomerAddressRepository.java
+++ src/main/java/com/example/zapbites/CustomerAddress/CustomerAddressRepository.java
@@ -2,5 +2,8 @@ package com.example.zapbites.CustomerAddress;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Long> {
+    Optional<CustomerAddress> findByAddress(String address);
 }
