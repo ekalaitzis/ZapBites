f63f38c42def06d9ee74957cf03111529994c80c
Ace <kalaitzisemmanouil@gmail.com>
2024-04-11 21:48:12 +0200

    add findAllByCustomer query

--- src/main/java/com/example/zapbites/CustomerAddress/CustomerAddressRepository.java
+++ src/main/java/com/example/zapbites/CustomerAddress/CustomerAddressRepository.java
@@ -1,9 +1,14 @@
 package com.example.zapbites.CustomerAddress;
 
+import com.example.zapbites.Customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Long> {
     Optional<CustomerAddress> findByAddress(String address);
+    @Query("SELECT ca FROM CustomerAddress ca WHERE ca.customer = :customer")
+    List<CustomerAddress> findAllByCustomer(Customer customer);
 }
