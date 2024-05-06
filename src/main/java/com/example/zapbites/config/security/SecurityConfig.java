3abd5aeebd4cf5d4e48617a28620d5b09de6a83f
Ace <kalaitzisemmanouil@gmail.com>
2024-05-06 19:43:43 +0200

    Refactor SecurityConfig

---
+++ src/main/java/com/example/zapbites/config/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.zapbites.config.security;
+
+import com.example.zapbites.Business.Security.BusinessUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final BusinessUserDetailsService businessUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(c -> c
+                .requestMatchers("/*/register").permitAll()
+                .requestMatchers("/business/***").hasRole("BUSINESS").anyRequest()
+                .authenticated());
+
+        http.csrf(c -> c.disable());
+
+
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
