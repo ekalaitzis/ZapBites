9641150f16391540c1903d8a39a011807e71f30d
Ace <kalaitzisemmanouil@gmail.com>
2024-06-02 21:19:35 +0200

    Add findAllByOrderId

--- src/main/java/com/example/zapbites/OrderProduct/OrderProductRepository.java
+++ src/main/java/com/example/zapbites/OrderProduct/OrderProductRepository.java
@@ -2,5 +2,8 @@ package com.example.zapbites.OrderProduct;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+    List<OrderProduct> findAllByOrderId(OrderProduct orderProduct);
 }
