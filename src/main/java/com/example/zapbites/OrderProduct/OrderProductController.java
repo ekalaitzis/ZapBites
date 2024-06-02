cc313729e7a14823740abc840e57667011648cea
Ace <kalaitzisemmanouil@gmail.com>
2024-06-02 21:16:30 +0200

    Add @PreAuthorize annotations to secure secure endpoints with evaluations

--- src/main/java/com/example/zapbites/OrderProduct/OrderProductController.java
+++ src/main/java/com/example/zapbites/OrderProduct/OrderProductController.java
@@ -1,11 +1,9 @@
 package com.example.zapbites.OrderProduct;
 
-import com.example.zapbites.OrderProduct.Exceptions.DuplicateOrderProductException;
-import com.example.zapbites.OrderProduct.Exceptions.OrderProductNotFoundException;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,30 +19,34 @@ public class OrderProductController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('BUSINESS', 'CUSTOMER')") //this is not secured properly as it is not going to be used in production
     public ResponseEntity<List<OrderProduct>> getAllOrderProducts() {
         List<OrderProduct> orderProducts = orderProductService.getAllOrderProducts();
         return new ResponseEntity<>(orderProducts, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("@ownerEvaluator.checkForOwnerByOrderProductId(#id)")
     public ResponseEntity<OrderProduct> getOrderProductById(@PathVariable Long id) {
         Optional<OrderProduct> optionalOrderProduct = orderProductService.getOrderProductById(id);
         return optionalOrderProduct.map(orderProduct -> new ResponseEntity<>(orderProduct, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Object> createOrderProduct(@Valid @RequestBody OrderProduct orderProduct) {
             OrderProduct createdOrderProduct = orderProductService.createOrderProduct(orderProduct);
             return new ResponseEntity<>(createdOrderProduct, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("@ownerEvaluator.checkForOwnerByOrderProduct(#orderProduct)")
     public ResponseEntity<OrderProduct> updateOrderProduct(@RequestBody OrderProduct orderProduct) {
             OrderProduct updatedOrderProduct = orderProductService.updateOrderProduct(orderProduct);
             return new ResponseEntity<>(updatedOrderProduct, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("@ownerEvaluator.checkForOwnerByOrderProductId(#id)")
     public ResponseEntity<Void> deleteOrderProductById(@PathVariable Long id) {
         orderProductService.deleteOrderProductById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/com/example/zapbites/OrderStatus/OrderStatusController.java
+++ src/main/java/com/example/zapbites/OrderStatus/OrderStatusController.java
@@ -1,11 +1,9 @@
 package com.example.zapbites.OrderStatus;
 
-import com.example.zapbites.OrderStatus.Exceptions.DuplicateOrderStatusException;
-import com.example.zapbites.OrderStatus.Exceptions.OrderStatusNotFoundException;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,30 +21,34 @@ public class OrderStatusController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('BUSINESS', 'CUSTOMER')") //this is mot secured properly as its not going to be used in production
     public ResponseEntity<List<OrderStatus>> getAllOrderStatuses() {
         List<OrderStatus> orderStatuses = orderStatusService.getAllOrderStatuses();
         return new ResponseEntity<>(orderStatuses, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("@ownerEvaluator.checkForOwnerByOrderStatusId(#id)")
     public ResponseEntity<OrderStatus> getOrderStatusById(@PathVariable Long id) {
         Optional<OrderStatus> optionalOrderStatus = orderStatusService.getOrderStatusById(id);
         return optionalOrderStatus.map(orderStatus -> new ResponseEntity<>(orderStatus, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Object> createOrderStatus(@Valid @RequestBody OrderStatus orderStatus) {
             OrderStatus createdOrderStatus = orderStatusService.createOrderStatus(orderStatus);
             return new ResponseEntity<>(createdOrderStatus, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("@ownerEvaluator.checkForOwnerByOrderStatus(#orderStatus)")
     public ResponseEntity<OrderStatus> updateOrderStatus(@RequestBody OrderStatus orderStatus) {
             OrderStatus updatedOrderStatus = orderStatusService.updateOrderStatus(orderStatus);
             return new ResponseEntity<>(updatedOrderStatus, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("@ownerEvaluator.checkForOwnerByOrderStatusId(#id)")
     public ResponseEntity<Void> deleteOrderStatusById(@PathVariable Long id) {
         orderStatusService.deleteOrderStatusById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
