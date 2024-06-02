0307033b455fab959220f658cf9e1d85c848f8a3
Ace <kalaitzisemmanouil@gmail.com>
2024-06-02 21:12:27 +0200

    Use Lombok to remove boilerplate code

--- src/main/java/com/example/zapbites/OrderProduct/OrderProduct.java
+++ src/main/java/com/example/zapbites/OrderProduct/OrderProduct.java
@@ -1,13 +1,19 @@
 package com.example.zapbites.OrderProduct;
 
-import com.example.zapbites.Orders.Orders;
+import com.example.zapbites.Order.Order;
 import com.example.zapbites.Product.Product;
 import jakarta.persistence.*;
-
-import java.util.Objects;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
 
 @Entity
 @Table(name = "order_product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class OrderProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_product_seq")
@@ -16,70 +22,11 @@ public class OrderProduct {
     private Long id;
     @ManyToOne
     @JoinColumn(name = "order_id", foreignKey = @ForeignKey(name = "fk_order"), nullable = false)
-    private Orders orderId;
+    private Order orderId;
+    @Positive
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
     @ManyToOne
     @JoinColumn(name = "product_id", foreignKey = @ForeignKey(name = "fk_product"))
     private Product product_id;
-
-    public OrderProduct() {
-    }
-
-    public OrderProduct(Long id, Orders orderId, Integer quantity, Product product_id) {
-        this.id = id;
-        this.orderId = orderId;
-        this.quantity = quantity;
-        this.product_id = product_id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Orders getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Orders orderId) {
-        this.orderId = orderId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Product getProduct_id() {
-        return product_id;
-    }
-
-    public void setProduct_id(Product product_id) {
-        this.product_id = product_id;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderProduct{" + "id=" + id + ", orderId=" + orderId + ", quantity=" + quantity + ", product_id=" + product_id + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderProduct that = (OrderProduct) o;
-        return Objects.equals(id, that.id) && Objects.equals(orderId, that.orderId) && Objects.equals(quantity, that.quantity) && Objects.equals(product_id, that.product_id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, orderId, quantity, product_id);
-    }
 }
--- src/main/java/com/example/zapbites/OrderStatus/OrderStatus.java
+++ src/main/java/com/example/zapbites/OrderStatus/OrderStatus.java
@@ -1,14 +1,19 @@
 package com.example.zapbites.OrderStatus;
 
-import com.example.zapbites.Orders.Orders;
+import com.example.zapbites.Order.Order;
 import jakarta.persistence.*;
-import org.hibernate.Session;
+import lombok.*;
 
 import java.sql.Timestamp;
-import java.util.Objects;
 
 @Entity
 @Table(name = "order_status")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class OrderStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_status_seq")
@@ -17,87 +22,15 @@ public class OrderStatus {
     private Long id;
     @OneToOne
     @JoinColumn(name = "order_id", foreignKey = @ForeignKey(name = "fk_order_status"), nullable = false)
-    private Orders orderId;
+    private Order orderId;
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
     private OrderStatusEnum orderStatusEnum;
     @Column(name = "session")
-    private Session session;
+    private String sessionId;
     @Column(name = "status_changed_at", nullable = false)
     private Timestamp statusChangedAt;
 
-    public OrderStatus() {
-    }
-
-    public OrderStatus(Long id, Orders orderId, OrderStatusEnum orderStatusEnum, Session session, Timestamp statusChangedAt) {
-        this.id = id;
-        this.orderId = orderId;
-        this.orderStatusEnum = orderStatusEnum;
-        this.session = session;
-        this.statusChangedAt = statusChangedAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Orders getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Orders orderId) {
-        this.orderId = orderId;
-    }
-
-    public OrderStatusEnum getOrderStatusEnum() {
-        return orderStatusEnum;
-    }
-
-    public void setOrderStatusEnum(OrderStatusEnum orderStatusEnum) {
-        this.orderStatusEnum = orderStatusEnum;
-    }
-
-    public Session getSession() {
-        return session;
-    }
-
-    public void setSession(Session session) {
-        this.session = session;
-    }
-
-    public Timestamp getStatusChangedAt() {
-        return statusChangedAt;
-    }
-
-    public void setStatusChangedAt(Timestamp statusChangedAt) {
-        this.statusChangedAt = statusChangedAt;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderStatus that = (OrderStatus) o;
-        return Objects.equals(id, that.id) && Objects.equals(orderId, that.orderId) && orderStatusEnum == that.orderStatusEnum && Objects.equals(session, that.session) && Objects.equals(statusChangedAt, that.statusChangedAt);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, orderId, orderStatusEnum, session, statusChangedAt);
-    }
-
-    @Override
-    public String toString() {
-        return "OrderStatus{" +
-                "id=" + id +
-                ", orderId=" + orderId +
-                ", orderStatusEnum=" + orderStatusEnum +
-                ", session=" + session +
-                ", statusChangedAt=" + statusChangedAt +
-                '}';
-    }
 }
+
+
